0dd008ced49720babb3a64278906fda1c1c1c90c
Taylor jr <luisdavidferrerconde@gmail.com>
2023-07-17 19:59:26 -0500

    desktop style all, done

--- modules/edit_product.js
+++ modules/edit_product.js
@@ -1,27 +1,30 @@
 import { productControl } from "./products_fetch.js";
 
+let category;
 const form = document.querySelector('[data-form]'),
       url = new URL(window.location),
       id = url.searchParams.get('id'),
       img = document.querySelector('[data-url]'),
-      category = document.querySelector('[data-category]'),
+      starwars = document.querySelector('[data-category-starwars]'),
+      consolas = document.querySelector('[data-category-consolas]'),
+      diversos = document.querySelector('[data-category-diversos]'),
       title = document.querySelector('[data-name]'),
       price = document.querySelector('[data-price]'),
       description = document.querySelector('[data-description]');
-      console.log()
+
 
 const infoPromise = async () => {
 
         if(id === null) window.location.href = '../screens/error.html';
    
         const product = await productControl.getInfo(id);
-        console.log(product)
         img.value = product.img;
-        category.value = product.category;
         title.value = product.title;
         price.value = product.price;      
-        description.value = product.description;     
-     
+        description.value = product.description;
+        
+        
+    
 }
 
 infoPromise();
@@ -30,10 +33,20 @@ form.addEventListener('submit', async e => {
     e.preventDefault();
 
     try {
-        await productControl.updateProduct(img.value,title.value,price.value,category.value,description.value,id);
+        switch (true) {
+        case starwars.checked:
+        category = "starwars";
+        break;
+        case consolas.checked:
+        category = "consolas";
+        break;
+        case diversos.checked:
+        category = "diversos";
+        break;
+        default:}
+        await productControl.updateProduct(img.value,title.value,price.value,category,description.value,id);
     } catch (error) {
         console.log(error)
     }
-
 })
     
